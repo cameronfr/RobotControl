47763f98814676cc0fd5408d3350ed321263da53
cameronfr <catechboy@hotmail.com>
2015-02-17 19:44:16 +0900

    minor refactoring

--- Ultrasonic.py
+++ Ultrasonic.py
@@ -7,7 +7,6 @@ class Ultrasonic:
     TIMEOUT = 0.020
 
     def __init__ (self, pin):
-        #GPIO.setmode(GPIO.BCM)
         self.setPin(pin);
 
     def setPin(self, pin):
@@ -16,29 +15,28 @@ class Ultrasonic:
     def read(self):
         GPIO.setup(self.pinNumber, GPIO.OUT)
         GPIO.output(self.pinNumber, 0)
-
         time.sleep(0.000002)
-        GPIO.output(pinNumber, 1)
+        GPIO.output(self.pinNumber, 1)
         time.sleep(0.000005)
-
         GPIO.output(self.pinNumber, 0)
         GPIO.setup(self.pinNumber, GPIO.IN)
 
-        goodread=True
-        watchtime=time.time()
-        while GPIO.input(self.pinNumber)==0 and goodread:
-                starttime=time.time()
-                if (starttime-watchtime > timeout):
-                        goodread=False
+        goodRead=True
+        if goodRead:
+            indexTime=time.time()
+            while GPIO.input(self.pinNumber)==0 and goodRead:
+                    startTime=time.time()
+                    if (startTime-indexTime > self.TIMEOUT):
+                            goodRead=False
 
-        if goodread:
-                watchtime=time.time()
-                while GPIO.input(self.pinNumber)==1 and goodread:
-                        endtime=time.time()
-                        if (endtime-watchtime > timeout):
-                                goodread=False
+        if goodRead:
+                indexTime=time.time()
+                while GPIO.input(self.pinNumber)==1 and goodRead:
+                        endTime=time.time()
+                        if (endTime-indexTime > self.TIMEOUT):
+                                goodRead=False
 
-        if goodread:
-                duration=endtime-starttime
+        if goodRead:
+                duration=endTime-startTime
                 distance=duration*34000/2
                 return distance
