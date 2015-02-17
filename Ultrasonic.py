59787817eacce576e790784c09a79ef31d8deb3b
cameronfr <catechboy@hotmail.com>
2015-02-17 16:15:12 +0900

    changed to class

--- Ultrasonic.py
+++ Ultrasonic.py
@@ -1,43 +1,44 @@
 import time
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
 
-timeout = 0.020
+class Ultrasonic:
 
+    TIMEOUT = 0.020
 
-while 1:
-        GPIO.setup(23, GPIO.OUT)
-        #cleanup output
-        GPIO.output(23, 0)
+    def __init__ (self, pin):
+        #GPIO.setmode(GPIO.BCM)
+        setPin(pin);
+
+    def setPin(pin):
+        self.pinNumber = pin
+
+    def read:
+        GPIO.setup(pinNumber, GPIO.OUT)
+        GPIO.output(pinNumber, 0)
 
         time.sleep(0.000002)
-
-        #send signal
-        GPIO.output(23, 1)
-
+        GPIO.output(pinNumber, 1)
         time.sleep(0.000005)
 
-        GPIO.output(23, 0)
-
-        GPIO.setup(23, GPIO.IN)
+        GPIO.output(pinNumber, 0)
+        GPIO.setup(pinNumber, GPIO.IN)
 
         goodread=True
         watchtime=time.time()
-        while GPIO.input(23)==0 and goodread:
+        while GPIO.input(pinNumber)==0 and goodread:
                 starttime=time.time()
                 if (starttime-watchtime > timeout):
                         goodread=False
 
         if goodread:
                 watchtime=time.time()
-                while GPIO.input(23)==1 and goodread:
+                while GPIO.input(pinNumber)==1 and goodread:
                         endtime=time.time()
                         if (endtime-watchtime > timeout):
                                 goodread=False
-                                print "badread"
 
         if goodread:
                 duration=endtime-starttime
                 distance=duration*34000/2
-                print distance
+                return distance
