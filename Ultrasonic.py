a4e63b657790c724c85480265cdad3af180e2c0f
cameronfr <catechboy@hotmail.com>
2015-02-26 16:27:30 +0900

    simplified read

--- Ultrasonic.py
+++ Ultrasonic.py
@@ -13,32 +13,23 @@ class Ultrasonic:
         self.pinNumber = pin
 
     def read(self):
+
         GPIO.setup(self.pinNumber, GPIO.OUT)
         GPIO.output(self.pinNumber, 0)
         time.sleep(0.000002)
+
         GPIO.output(self.pinNumber, 1)
         time.sleep(0.000005)
         GPIO.output(self.pinNumber, 0)
+
         GPIO.setup(self.pinNumber, GPIO.IN)
 
-        goodRead=True
-        if goodRead:
-            indexTime=time.time()
-            while GPIO.input(self.pinNumber)==0 and goodRead:
-                    startTime=time.time()
-                    if (startTime-indexTime > self.TIMEOUT):
-                            goodRead=False
+        while GPIO.input(self.pinNumber)==0:
+            starttime=time.time()
 
-        if goodRead:
-                indexTime=time.time()
-                while GPIO.input(self.pinNumber)==1 and goodRead:
-                        endTime=time.time()
-                        if (endTime-indexTime > self.TIMEOUT):
-                                goodRead=False
+        while GPIO.input(self.pinNumber)==1:
+            endtime=time.time()
 
-        if goodRead:
-                duration=endTime-startTime
-                distance=duration*34000/2
-                return distance
-	else:
-		return 0
+        duration=endtime-starttime
+        distance=duration*34000/2
+        return distance
