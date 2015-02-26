65a49fd6a34fb04007e31cd1a0b499b88d99233d
Cameron Franz <catechboy@hotmail.com>
2015-02-26 07:17:21 +0000

    minor fixes

--- Controller.py
+++ Controller.py
@@ -1,9 +1,11 @@
 import Motor
 import Ultrasonic
+import RPi.GPIO as GPIO
 
 if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
     motor = Motor.Motor(23)
     sonar = Ultrasonic.Ultrasonic(24)
     motor.initialize(1600)
     while(True):
-        motor.setSpeed(sonar.read*5)
+        motor.setSpeed(sonar.read()*5)
