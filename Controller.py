89f35defdeeaf6e3014b4d931c7a05e32de12688
Cameron Franz <catechboy@hotmail.com>
2015-06-01 01:59:38 +0000

    added L and R motors

--- Controller.py
+++ Controller.py
@@ -2,10 +2,16 @@ import Motor
 import Ultrasonic
 import RPi.GPIO as GPIO
 
-if __name__ == '__main__':
-    GPIO.setmode(GPIO.BCM)
-    motor = Motor.Motor(23)
-    sonar = Ultrasonic.Ultrasonic(24)
-    motor.initialize(1600)
-    while(True):
-        motor.setSpeed(sonar.read()*5)
+
+class Controller:
+
+    LEFT_MOTOR_PORT = 7
+    RIGHT_MOTOR_PORT = 15
+    START_FREQ = 1600
+
+    def __init__ (self):
+        GPIO.setmode(GPIO.BCM)
+        self.leftMotor = Motor.Motor(self.LEFT_MOTOR_PORT)
+        self.rightMotor = Motor.Motor(self.RIGHT_MOTOR_PORT)
+        self.leftMotor.initialize(self.START_FREQ)
+        self.rightMotor.initialize(self.START_FREQ)
