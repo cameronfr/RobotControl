cd421cc3485853868ae572aa0ca38a55a13a077b
Cameron Franz <catechboy@hotmail.com>
2015-12-19 13:47:00 +0000

    added basic shoulder joint control

--- Control.py
+++ Control.py
@@ -1,64 +1,62 @@
 #!/usr/bin/python
 
-from Adafruit_PWM_Servo_Driver import PWM
+from lib.Adafruit_PWM_Servo_Driver import PWM
 import time
 
 pwm = PWM(0x40)
 
 #Note: configure these later
-SERVOMIN = 1400  # Min pulse length out in ms
-SERVOMAX = 1600  # Max pulse length out in ms
+SERVOMIN = 500  # Min pulse length out in us
+SERVOMAX = 2500  # Max pulse length out in us
 PWMFREQ = 60
 
 #Servo Port configuration
-botLeftShoulderServo = 0;
-botRightShoulderServo = 1;
-topLeftShoulderServo = 2;
-topRightShoulderServo = 3;
+topHorizShoulderServo = 0;
+botHorizShoulderServo = 1;
+topVertShoulderServo = 2;
+botVertShoulderServo = 3;
 
 #For Demo Purposes
-currentBottomAngle = 90;
-currentTopAngle = 90;
+currentHorizAngle = 90;
+currentVertAngle = 90;
 
 #takes pulse in ms and outputs to servo channel (modified from example)
 def setServoPulse(channel, pulse):
   pulseLength = 1000000                   # 1,000,000 us per second
   pulseLength /= PWMFREQ                       # 60 Hz
-  print "%d us per period" % pulseLength
+  #print "%d us per period" % pulseLength
   pulseLength /= 4096                     # 12 bits of resolution
-  print "%d us per bit" % pulseLength
+  #print "%d us per bit" % pulseLength 				
   pulse /= pulseLength
+  pulse = int(pulse)
   pwm.setPWM(channel, 0, pulse)
 
 def init():
-    pwm.setPWMFreq(PWMFREQ)                        # Set frequency to 60 Hz
+	pwm.setPWMFreq(PWMFREQ)                        # Set frequency to 60 Hz
 
-def moveBottomShoulder(angle):
-    pulselen = map(angle, 0, 180, SERVOMIN, SERVOMAX)
-    setServoPulse(botLeftShoulderServo,pulselen)
-    setServoPulse(botLeftShoulderServo,pulselen)
+def angleToPulse(angle):
+	return (((angle/180.0) * (SERVOMAX-SERVOMIN)) + SERVOMIN)
 
-def moveBottomShoulder(angle):
-    pulselen = map(angle, 0, 180, SERVOMIN, SERVOMAX)
-    setServoPulse(botLeftShoulderServo,pulselen)
-    setServoPulse(botLeftShoulderServo,pulselen)
+def moveHorizShoulder(angle):
+	setServoPulse(topHorizShoulderServo,angleToPulse(angle))
+	setServoPulse(botHorizShoulderServo,angleToPulse(180-angle))
 
-def moveTopShoulder(angle):
-    pulselen = map(angle, 0, 180, SERVOMIN, SERVOMAX)
-    setServoPulse(topLeftShoulderServo,pulselen)
-    setServoPulse(topLeftShoulderServo,pulselen)
+def moveVertShoulder(angle):
+	setServoPulse(topVertShoulderServo,angleToPulse(angle))
+	setServoPulse(botVertShoulderServo,angleToPulse(180-angle))
 
 init()
 #for easy testing
 while (True):
-    direction = input("W or A or S or D").lower();
+    direction = raw_input("W or A or S or D").lower();
     if direction == "a":
-        currentBottomAngle = max(currentBottomAngle - 1,0);
-    elif direction == "d"
-        currentBottomAngle = min(currentBottomAngle + 1,180);
+        currentHorizAngle = max(currentHorizAngle - 30,0);
+    elif direction == "d":
+        currentHorizAngle = min(currentHorizAngle + 30,180);
     elif direction == "w":
-        currentTopAngle = min(currentTopAngle + 1,180);
+        currentVertAngle = min(currentVertAngle + 30,180);
     elif direction == "s":
-        currentTopAngle = max(currentTopAngle - 1,0);
-    moveBottomShoulder(currentBottomAngle)
-    moveTopShoulder(currentTopAngle)
+        currentVertAngle = max(currentVertAngle - 30,0);
+
+    moveHorizShoulder(currentHorizAngle)
+    moveVertShoulder(currentVertAngle)
