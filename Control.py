6b74101ac629daebbfe27fc51c5981e55e81c928
Cameron Franz <catechboy@hotmail.com>
2015-12-21 03:40:32 +0000

    implemented all joints in the basic test program

--- Control.py
+++ Control.py
@@ -9,23 +9,35 @@ def main():
     elbowJoint = Arm.Joint([Arm.Servo(4)],[1])
     clawJoint = Arm.Joint([Arm.Servo(5),Arm.Servo(6)],[1,-1])
 
-    currentHorizAngle = 90;
-    currentVertAngle = 90;
+    currentHorizAngle = 90
+    currentVertAngle = 90
+    currentElbowAngle = 90
+    currentClawAngle = 90
 
     #for easy testing
     while (True):
-        direction = raw_input("W or A or S or D").lower();
+        direction = raw_input("W, A, S, D, Q, E, G, R: ").lower();
         if direction == "a":
-            currentHorizAngle = max(currentHorizAngle - 30,0);
+            currentHorizAngle = max(currentHorizAngle - 10,0);
         elif direction == "d":
-            currentHorizAngle = min(currentHorizAngle + 30,180);
-        elif direction == "w":
-            currentVertAngle = min(currentVertAngle + 30,180);
+            currentHorizAngle = min(currentHorizAngle + 10,180);
+        if direction == "w":
+            currentVertAngle = min(currentVertAngle + 10,180);
         elif direction == "s":
-            currentVertAngle = max(currentVertAngle - 30,0);
+            currentVertAngle = max(currentVertAngle - 10,0);
+	if direction == "q":
+	    currentElbowAngle = min(currentElbowAngle + 10, 180)
+	elif direction == "e":
+	    currentElbowAngle = max(currentElbowAngle - 10, 0)
+	if direction == "g":
+	    currentClawAngle = min(currentClawAngle + 10, 180)
+	elif direction == "r":
+	    currentClawAngle = max(currentClawAngle - 10, 0)  
 
         horizJoint.moveToAngle(currentHorizAngle)
         vertJoint.moveToAngle(currentVertAngle)
+	elbowJoint.moveToAngle(currentElbowAngle)
+	clawJoint.moveToAngle(currentClawAngle)
 
 if __name__ == "__main__":
     main()
