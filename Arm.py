fa34cc8e7bcafcb719a5983e9dbb41f3edb7179e
Cameron Franz <catechboy@hotmail.com>
2015-12-21 03:04:02 +0000

    fixed integer division causing pulse length errors

--- Arm.py
+++ Arm.py
@@ -6,32 +6,33 @@ class Servo:
     pwm = PWM(0x40) #may need to move later
     SERVOMIN = 500  # min pulse length out in us
     SERVOMAX = 2500  # max pulse length out in us
-    ANGLEMIN = 0
-    ANGLEMAX = 180
+    ANGLEMIN = 0.0
+    ANGLEMAX = 180.0
     PWMFREQ = 60
 
     def __init__(self, channel):
         Servo.pwm.setPWMFreq(Servo.PWMFREQ)
         self.channel = channel
 
-    def setServoPulse(self, channel, pulse):
+    def setServoPulse(self, pulse):
+	print("call: pwm: channel:" + str(self.channel) + " pulse:" + str(pulse))	
         pulseLength = 1000000                   # 1,000,000 us per second
         pulseLength /= Servo.PWMFREQ                       # 60 Hz
         pulseLength /= 4096                     # 12 bits of resolution
         pulse /= pulseLength
         pulse = int(pulse)
-        Servo.pwm.setPWM(channel, 0, pulse)
+        Servo.pwm.setPWM(self.channel, 0, pulse)
 
     def angleToPulse(self, angle):
-        return ((((angle-Servo.ANGLEMIN)/(Servo.ANGLEMAX-Servo.ANGLEMIN)) * (Servo.SERVOMAX-Servo.SERVOMIN)) + Servo.SERVOMIN)
+        return ((((float(angle)-Servo.ANGLEMIN)/(Servo.ANGLEMAX-Servo.ANGLEMIN)) * (Servo.SERVOMAX-Servo.SERVOMIN)) + Servo.SERVOMIN)
 
     def moveToAngle(self, angle, reversed = False):
         if not reversed:
-            self.setServoPulse(self.channel,self.angleToPulse(angle))
-	    #print("call: channel:" + str(self.channel) + " angle:" + str(angle)) 
+            self.setServoPulse(self.angleToPulse(angle))
+	    print("call: move: channel:" + str(self.channel) + " angle:" + str(angle)) 
         else:
-            self.setServoPulse(self.channel,self.angleToPulse(self.reverseAngle(angle)))
-	    #print("call: channel:" + str(self.channel) + " angle:" + str(self.reverseAngle(angle)))
+            self.setServoPulse(self.angleToPulse(self.reverseAngle(angle)))
+	    print("call: move: channel:" + str(self.channel) + " angle:" + str(self.reverseAngle(angle)))
   
     def reverseAngle(self, angle):
         return (Servo.ANGLEMIN + (Servo.ANGLEMAX - angle))
